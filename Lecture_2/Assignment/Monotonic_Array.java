a8884de09d3e6074b1fdc909dbd7022a1fec6478
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-06-24 12:28:39 +0530

    Monotonic Array

---
+++ Lecture_2/Assignment/Monotonic_Array.java
@@ -0,0 +1,22 @@
+public class Monotonic_Array {
+    public static boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 3};
+        boolean isMonotonic = isMonotonic(nums);
+        System.out.println(isMonotonic);
+    }
+}
