210119905b1c42cac3e9c1f0a29c172bca4cabcd
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-06-25 11:45:07 +0530

    First Unique Character in a String

--- Mock Test 1/First_Unique_Character_in_a_String.java
+++ Mock Test 1/First_Unique_Character_in_a_String.java
@@ -1,3 +1,23 @@
+//First Unique Character in a String
+//
+//        Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+//
+//        Example 1:
+//        Input: s = "leetcode"
+//        Output: 0
+//
+//        Example 2:
+//        Input: s = "loveleetcode"
+//        Output: 2
+//
+//        Example 3:
+//        Input: s = "aabb"
+//        Output: -1
+//
+//        Constraints:
+//        a. 1 <= s.length <= 10^5
+//        b. s consists of only lowercase English letters.
+
 public class First_Unique_Character_in_a_String {
     public static int firstUniqChar(String s) {
         int[] count = new int[26];
