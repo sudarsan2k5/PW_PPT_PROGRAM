5f682d81d8cb5ba56e7d280460cebf1fdfcaffc3
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-06-25 11:20:31 +0530

    First Unique Character in a String

---
+++ Mock Test 1/First_Unique_Character_in_a_String.java
@@ -0,0 +1,25 @@
+public class First_Unique_Character_in_a_String {
+    public static int firstUniqChar(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            count[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (count[c - 'a'] == 1) {
+                System.out.println("Non Repeating character: " + c);
+                return i;
+            }
+        }
+
+        System.out.println("No Repeating Character Found: ");
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(firstUniqChar(s));
+    }
+}
