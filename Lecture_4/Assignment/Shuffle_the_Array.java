753aec1181eea611fcbfed282c3dbc0be35f4814
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-06-28 10:02:57 +0530

    Shuffle the Array

---
+++ Lecture_4/Assignment/Shuffle_the_Array.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class Shuffle_the_Array {
+    public static int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            result[index++] = nums[i];
+            result[index++] = nums[i + n];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 1, 3, 4, 7};
+        int n = 3;
+        int[] shuffled = shuffle(nums, n);
+        System.out.println(Arrays.toString(shuffled));
+    }
+}
