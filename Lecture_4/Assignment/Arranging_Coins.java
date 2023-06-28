fd732ebdebb8e8f5111f17bef01c0fb087bb8fa1
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-06-28 09:58:49 +0530

    Squares of a Sorted Array

--- Lecture_4/Assignment/Arranging_Coins.java
+++ Lecture_4/Assignment/Arranging_Coins.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Arranging_Coins {
+public class Arranging_Coins {
+    public static int arrangeCoins(int n) {
+        long left = 0;
+        long right = n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long currSum = (mid * (mid + 1)) / 2;
+
+            if (currSum == n) {
+                return (int) mid;
+            } else if (currSum < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) right;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int completeRows = arrangeCoins(n);
+        System.out.println(completeRows);
+    }
 }
