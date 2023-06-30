c378b1a0f1fff3ead6e040463966905989f2338a
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-07-01 01:36:17 +0530

    Contiguous Array

---
+++ Lecture_6/Assignment_6/Contiguous_Array.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contiguous_Array {
+    public static int findMaxLength(int[] nums) {
+        int maxLength = 0;
+        int count = 0;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        countMap.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count--;
+            } else {
+                count++;
+            }
+
+            if (countMap.containsKey(count)) {
+                int prevIndex = countMap.get(count);
+                maxLength = Math.max(maxLength, i - prevIndex);
+            } else {
+                countMap.put(count, i);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1};
+        int maxLength = findMaxLength(nums);
+        System.out.println(maxLength);
+    }
+}
