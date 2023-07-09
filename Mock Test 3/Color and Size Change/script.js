aeba4c1965143cc1b7b66cbd397a576006868e7c
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2023-07-09 21:58:06 +0530

    Color and Size Change

---
+++ Mock Test 3/Color and Size Change/script.js
@@ -0,0 +1,11 @@
+const box = document.querySelector('.box');
+let isYellow = false;
+
+box.addEventListener('click', function() {
+  if (!isYellow) {
+    box.style.backgroundColor = 'yellow';
+    box.style.width = '600px';
+    box.style.height = '600px';
+    isYellow = true;
+  }
+});
